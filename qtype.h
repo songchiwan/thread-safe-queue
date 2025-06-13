32f215a093b73ead8416cf250f1df9deaf23e418
songchiwan <37866777+songchiwan@users.noreply.github.com>
2025-06-13 22:44:58 +0900

    qtype.h
    Queue 부분에 mutex를 추가했습니다.

---
+++ qtype.h
@@ -0,0 +1,38 @@
+#ifndef _QTYPE_H  // header guard
+#define _QTYPE_H
+#include <mutex>
+
+
+
+// ==========이 파일은 수정 가능==========
+
+typedef unsigned int Key;  // 값이 클수록 높은 우선순위
+typedef void* Value;
+
+typedef struct {
+    Key key;
+    Value value;
+    int value_size;  // value가 가리키는 메모리 크기 (바이트 단위)
+} Item;
+
+typedef struct {
+    bool success;   // true: 성공, false: 실패
+    Item item;
+    // 필드 추가 가능
+} Reply;
+
+typedef struct node_t {
+    Item item;
+    struct node_t* next;
+    // 필드 추가 가능
+} Node;
+
+typedef struct {
+    Node* head, * tail;
+    std::mutex mtx;
+    // 필드 추가 가능
+} Queue;
+
+// 이후 자유롭게 추가/수정: 새로운 자료형 정의 등
+
+#endif
